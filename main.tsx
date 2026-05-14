ffc07983962c11f371310d7089a6bda1fd8ff76e
ghorm19-spec <ghorm19@gmail.com>
2026-05-14 10:18:56 -0400

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
-import './index.css'
+import './src/styles.css'
 
 const router = createRouter({ routeTree })
 
